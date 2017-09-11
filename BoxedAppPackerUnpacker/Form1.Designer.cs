f5983b8783c031873ad6ffbb8c2e42b0194d056b
MindSystemm <mindlockreverser@gmail.com>
2017-09-11 11:54:56 +0200

    Initial Release
    Tool to unpack .net assembly packed with BoxedAppPacker.
    Thanks to atom0s for his SteamLess library

---
+++ BoxedAppPackerUnpacker/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace BoxedAppPackerUnpacker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(214, 162);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(205, 38);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Exit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(251, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(143, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Status : Waiting for assembly";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(232, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(172, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Drag your assembly into the picture";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(196, 192);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.TextBox1DragDrop);
+            this.pictureBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.TextBox1DragEnter);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(254, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "MZ found in : ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(436, 220);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "BoxedAppUnpacked by MindSystem";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ BoxedAppPackerUnpacker/Form1.cs
@@ -0,0 +1,229 @@
+﻿using Steamless.API.PE32;
+using Steamless.API.PE64;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BoxedAppPackerUnpacker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.AllowDrop = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        
+        }
+        public uint GetAlignment(uint val, uint align)
+        {
+            return (((val + align - 1) / align) * align);
+        }
+        
+        /// <summary>
+        /// Gets or sets the raw file data read from disk.
+        /// </summary>
+        public byte[] FileData { get; set; }
+
+        /// <summary>
+        /// Gets or sets the dos header of the file.
+        /// </summary>
+        public NativeApi32.ImageDosHeader32 DosHeader32 { get; set; }
+
+        /// <summary>
+        /// Gets or sets the NT headers of the file.
+        /// </summary>
+        public NativeApi32.ImageNtHeaders32 NtHeaders32 { get; set; }
+
+        /// <summary>
+        /// Gets or sets the sections of the file.
+        /// </summary>
+        public List<NativeApi32.ImageSectionHeader32> Sections32;
+
+        /// <summary>
+        /// Gets or sets the section data of the file.
+        /// </summary>
+        public List<byte[]> SectionData;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private void TextBox1DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+        string DirectoryName = "";
+        string ExePath = "";
+        private void TextBox1DragDrop(object sender, DragEventArgs e)
+        {
+            try
+            {
+                Array arrayyy = (Array)e.Data.GetData(DataFormats.FileDrop);
+                if (arrayyy != null)
+                {
+                    string text = arrayyy.GetValue(0).ToString();
+                    int num = text.LastIndexOf(".", StringComparison.Ordinal);
+                    if (num != -1)
+                    {
+                        string text2 = text.Substring(num);
+                        text2 = text2.ToLower();
+                        if (text2 == ".exe" || text2 == ".dll")
+                        {
+                            Activate();
+                            ExePath = text;
+                            label2.Text = "Status : Exe Loaded";
+                            int num2 = text.LastIndexOf("\\", StringComparison.Ordinal);
+                            if (num2 != -1)
+                            {
+                                DirectoryName = text.Remove(num2, text.Length - num2);
+                            }
+                            if (DirectoryName.Length == 2)
+                            {
+                                DirectoryName += "\\";
+                            }
+                        }
+                    }
+                }
+            }
+            catch
+            {
+            }
+            this.FileData = File.ReadAllBytes(ExePath);
+            if (IntPtr.Size == 4)
+            {
+                this.NtHeaders32 = new NativeApi32.ImageNtHeaders32();
+                this.DosHeader32 = new NativeApi32.ImageDosHeader32();
+                this.DosHeader32 = Pe32Helpers.GetStructure<NativeApi32.ImageDosHeader32>(this.FileData);
+                this.NtHeaders32 = Pe32Helpers.GetStructure<NativeApi32.ImageNtHeaders32>(this.FileData, this.DosHeader32.e_lfanew);
+                int num = NtHeaders32.FileHeader.NumberOfSections;
+                for (var x = 0; x < num; x++)
+                {
+                    var section = Pe32Helpers.GetSection(this.FileData, x, this.DosHeader32, this.NtHeaders32);
+                    if (section.SectionName.Equals(".bxpck"))
+                    {
+                        var sectionData = new byte[this.GetAlignment(section.SizeOfRawData, this.NtHeaders32.OptionalHeader.FileAlignment)];
+                        Array.Copy(this.FileData, section.PointerToRawData, sectionData, 0, section.SizeOfRawData);
+                        string filename = DirectoryName + "\\" + Path.GetFileNameWithoutExtension(ExePath) + "-Unpacked" + Path.GetExtension(ExePath);
+
+                        int firstIndex = GetNthIndex(sectionData, 0x5A, 1);
+                        if (sectionData[firstIndex - 1] == 0x4D)
+                        {
+                            File.WriteAllBytes(filename, sectionData.Skip(firstIndex - 1).ToArray());
+                            label3.Text += (firstIndex-1).ToString("X");
+                        }
+                        else
+                        {
+                            int secondIndex = GetNthIndex(sectionData, 0x5A, 2);
+                            if (sectionData[secondIndex - 1] == 0x4A)
+                            {
+                                File.WriteAllBytes(filename, sectionData.Skip(secondIndex - 1).ToArray());
+                                label3.Text += (secondIndex-1).ToString("X");
+                            }
+                            else
+                            {
+                                int lastIndex = GetNthIndex(sectionData, 0x4D, 3);
+                                if (sectionData[lastIndex - 1] == 0x4D)
+                                {
+                                    File.WriteAllBytes(filename, sectionData.Skip(lastIndex - 1).ToArray());
+                                    label3.Text += (lastIndex - 1).ToString("X");
+                                }
+                                else
+                                {
+
+                                }
+                            }
+                        }
+                        goto sucess;
+                    }
+                }
+
+            }
+            else
+            {
+                this.DosHeader64 = new NativeApi64.ImageDosHeader64();
+                this.NtHeaders64 = new NativeApi64.ImageNtHeaders64();
+                this.DosHeader64 = Pe64Helpers.GetStructure<NativeApi64.ImageDosHeader64>(this.FileData);
+                this.NtHeaders64 = Pe64Helpers.GetStructure<NativeApi64.ImageNtHeaders64>(this.FileData, this.DosHeader64.e_lfanew);
+                for (var x = 0; x < this.NtHeaders64.FileHeader.NumberOfSections; x++)
+                {
+                    var section = Pe64Helpers.GetSection(this.FileData, x, this.DosHeader64, this.NtHeaders64);
+                    if (section.SectionName.Equals(".bxpck"))
+                    {
+                        var sectionData = new byte[this.GetAlignment(section.SizeOfRawData, this.NtHeaders64.OptionalHeader.FileAlignment)];
+                        Array.Copy(this.FileData, section.PointerToRawData, sectionData, 0, section.SizeOfRawData);
+                        string filename = DirectoryName + "\\" + Path.GetFileNameWithoutExtension(ExePath) + "-Unpacked" + Path.GetExtension(ExePath);
+                        File.WriteAllBytes(filename, sectionData.Skip(182).ToArray());
+                        goto sucess;
+                    }
+                }
+              
+            }
+            MessageBox.Show("BoxedAppPacker section not found (.bxpck) ! ", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            sucess:
+            label2.Text = "Status : Success ! ";
+        }
+        public int GetNthIndex(byte[] s, byte t, int n)
+        {
+            int count = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == t)
+                {
+                    count++;
+                    if (count == n)
+                    {
+                        return i;
+                    }
+                }
+            }
+            return -1;
+        }
+        public static int GetFirstOccurance(byte[] byteArray, byte byteToFind)
+        {
+            return Array.IndexOf(byteArray, byteToFind);
+        }
+        /// <summary>
+        /// Gets or sets the dos header of the file.
+        /// </summary>
+        public NativeApi64.ImageDosHeader64 DosHeader64 { get; set; }
+
+        /// <summary>
+        /// Gets or sets the NT headers of the file.
+        /// </summary>
+        public NativeApi64.ImageNtHeaders64 NtHeaders64 { get; set; }
+
+
+        /// <summary>
+        /// Gets or sets the sections of the file.
+        /// </summary>
+        public List<NativeApi64.ImageSectionHeader64> Sections64;
+        /// <summary>
+        /// Gets or sets the Tls directory of the file.
+        /// </summary>
+        public NativeApi64.ImageTlsDirectory64 TlsDirectory64 { get; set; }
+
+        /// <summary>
+        /// Gets or sets a list of Tls callbacks of the file.
+        /// </summary>
+        public List<ulong> TlsCallbacks64 { get; set; }
+    }
+}
